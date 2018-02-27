cffe38a425ef9ba34d0296e59b91ecc4a023e8a5
Aques <36673324+Aques@users.noreply.github.com>
2018-02-27 23:44:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
-console.log('Loaded!');
-element = document.getElementById('main-text')
-element.innerHTML = 'XD Learning to use javascript'
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+ 
+ //Create a request
+ 
+ //Capture the request
+ 
+ //Rendering the variable in correct span
+ counter = counter + 1;
+ var span= document.getElementById('count');
+ span.innerHTML = counter.toString();
+};
