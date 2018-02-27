b69b2575c5d18d0a82f6ecb69e766ea0959ee58f
Aques <36673324+Aques@users.noreply.github.com>
2018-02-28 01:14:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,9 @@
-//Counter code
-var button = document.getElementById('counter');
-button.onclick = function(){
- 
- //Create a request
- var request = new XMLHttpRequest();
- 
- //Capture the request
- request.onreadystatechange = function(){
-     if(request.readyState === XMLHttpRequest.DONE){
-         //Take some action
-         if(request.status === 200){
-             var counter = request.responseText;
-             var span= document.getElementById('count');
-             span.innerHTML = counter.toString();
-             
-         }
-         
-     }
-     
- };
- //Make a request
-        request.open('GET', 'http://prashantbhambhanik.imad.hasura-app.io/counter', true);
-        request.send(null);
- 
- 
-};
+
 //Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit-button');
-submit.onclick = fucntion ()
-{
+submit.onclick = fucntion(){
     //Make request to server and send the name.
     
     // Capture a list of names and render it as list.
