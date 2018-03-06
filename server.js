e929c5b9e44af8666bbc2300dff81e80229b38d7
Aques <36673324+Aques@users.noreply.github.com>
2018-03-07 00:04:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
   
-    pool.query('SELECT * from "users" username = $1', [username], function(err, result){
+    pool.query('SELECT * FROM "users" WHERE username = $1', [username], function(err, result){
          if(err){
             res.status(500).send(err, tostring());
         }
